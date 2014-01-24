07adfa37fa6cc6ac51dc5a1a86f86f5f5c06f25d
macbric <macbric>
2014-01-24 16:26:28 +0100

    Clean

--- server/src/com/h4313/deephouse/server/main/Main.java
+++ server/src/com/h4313/deephouse/server/main/Main.java
@@ -2,52 +2,13 @@ package com.h4313.deephouse.server.main;
 
 import java.util.Scanner;
 
-import com.h4313.deephouse.network.CallBack;
 import com.h4313.deephouse.network.TcpReceiveExample;
 import com.h4313.deephouse.network.TcpSendExample;
-import com.h4313.deephouse.network.TcpSender;
-import com.h4313.deephouse.util.Tool;
 
 public class Main {
 	public static void main(String[] args)
 	{
-
-		String str = new String();
-		if (args.length == 0)
-		{
-			try 
-			{
-				
-				str = "";
-				Scanner scIp;
-				do {
-					scIp = new Scanner(System.in);
-					System.out.println("Quelle est l'adresse du proxy ?");
-					str = scIp.nextLine();
-				} while (!Tool.isValidIp(str));
-				scIp.close();
-
-				String ip = str;
-
-				str = "";
-				Scanner scPort;
-				do {
-					scPort = new Scanner(System.in);
-					System.out.println("Sur quel port est connecté le proxy ?");
-					str = scPort.nextLine();
-					System.out.println("str=" + str);
-				} while (!Tool.isValidPort(str));
-				scPort.close();
-				
-				int port = Integer.valueOf(str).intValue();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-				 System.out.println("Erreur de l'application : " + e.getMessage());
-			}
-		}
-		else if(args.length == 1)
+		if(args.length == 1)
 		{
 			int port = Integer.valueOf(args[0]);
 			new TcpReceiveExample(port);
@@ -59,10 +20,9 @@ public class Main {
 			new TcpSendExample(ip, port);
 		}
 		
-		
 			/************************/
 		
-			str = "";
+			String str = "";
 			Scanner scExit;
 			do {
 				scExit = new Scanner(System.in);
